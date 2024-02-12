959ce0574ceef36a13906f7cb8108b9351552435
filip wozniak <filip.wozniak99@gmail.com>
2024-02-12 14:28:03 +0100

    feat: compiler cleanup + page styling

--- client/src/components/compilers/Compiler.jsx
+++ client/src/components/compilers/Compiler.jsx
@@ -1,24 +1,32 @@
-/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import { useEffect } from 'react'
+import './Compiler.scss'
 
 const Compiler = () => {
+
   useEffect(() => {
     const SEC_HTTPS = true;
     const SEC_BASE = "compilers.widgets.sphere-engine.com";
-    (function(d, s, id){ SEC = window.SEC || (window.SEC = []);
+    (function(d, s, id){ let SEC = window.SEC || (window.SEC = []);
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return; js = d.createElement(s); js.id = id;
       js.src = (SEC_HTTPS ? "https" : "http") + "://" + SEC_BASE + "/static/sdk/sdk.min.js";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, "script", "sphere-engine-compilers-jssdk"));
+
+    return () => {
+      const scriptElement = document.getElementById("sphere-engine-compilers-jssdk");
+      scriptElement.parentNode.removeChild(scriptElement);
+    };
   }, []);
 
-
   return (
-      <>
-        <h1>Compilers</h1>
-        <div className="sec-widget" data-widget={import.meta.env.VITE_WIDGET_HASH}></div>
-      </>
+      <div className="compiler-page">
+        <h1>Compiler</h1>
+        <div className="widget-container">
+          <div className="sec-widget" data-widget={import.meta.env.VITE_WIDGET_HASH}></div>
+        </div>
+      </div>
   );
 }
 
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,21 +1,22 @@
 import { Link } from "react-router-dom"
+import sphereLogo from'../../assets/sphere-logo-typography.svg'
+import './Navbar.scss'
 
 const Navbar = () => {
 
-
   return (
-      <>
-        <div>
-            <Link to="/" >Sphere</Link>
+      <div className="navbar">
+        <div className="logo">
+            <Link to="/" ><img src={sphereLogo}/></Link>
         </div>
-        <div>
+        <div className="navlinks-container">
           <Link to="/compilers">Compilers</Link>
 
           <Link to="/problems">Problems</Link>
 
           <Link to="/containers">Containers</Link>
         </div>
-      </>
+      </div>
   );
 }
 
